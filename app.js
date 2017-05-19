98971f1dc58b05e493273d18f1dbfc6c2dbd03b4
Romain32190 <romain.lafargue36@gmail.com>
2017-05-19 12:13:05 +0200

    Ajout bouton supprimer pour les articles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var textarea = $("textarea").val("");
 var input = $("input").val("");
 
-function afficher(){
+function afficher(article){
 
 	$.ajax({ 
 		url:'/afficher', 
@@ -18,25 +18,63 @@ function afficher(){
 			$("#title").append('<div><option value="'+i+'">'+data[i].titre+'</option></div>');
 			$("#article").append('<div class="ID'+i+'">'+data[i].text+'</div>');
 			$('#title').html();
-			$('#contenu').append('<li >'+data[i].titre+' <button  value="'+data[i]._id+'" class="waves-effect waves-light btn-small red supprimer">Supprimer</button><button value="'+data[i]._id+'" class="waves-effect waves-light btn-small blue modifier">Modifier</button></li>');
-		}
-		$('.supprimer').click(function(){
-			var id = $(this).attr('value');
-			console.log(id);
+			$('#contenu').append('<li role="presentation"><a id="contenu" value="'+i+'" href="#">'+data[i].titre+'</a></li> <button  data-id="'+data[i].id+'" class="waves-effect waves-light btn-small red supprimer">Supprimer</button><button value="'+data[i].id+'" class="waves-effect waves-light btn-small blue modifier">Modifier</button></li>');
 
-			$.ajax({
+
+		}
+
+		$("body").delegate('.supprimer', 'click', function(e) {
+			e.preventDefault();
+			console.log('Yaaay');
+			var id = $(this).data('id');
+			console.log(id);
+			$.ajax({  
 				url:'/delete',
+				method: 'POST',
 				data: {
-					_id: id,
+					id: id,
 				}
+			}).done(function(e){
+				console.log(e);
 			});
 
 		});
-
 	});
 }
 
+
+$('#contenu').click(function(){
+var ind = $(this).attr('value');
+console.log(ind);
+$("#inputModif").val($("#text"));
+$("#textareaModif").val($("#titre"));
+});
+
+
+$("body").delegate('.modifier', 'click', function(e) {
+	e.preventDefault();
+	var modifier = {"titremodif" : $("#inputModif").val(), "textmodif" : $("#textareaModif").val()};
+	var idmodif= $(this).data('id');
+	console.log(idmodif);
+	
+	$("#inputModif").val("");
+	$("#textareaModif").val("");
+	
+	$.ajax({
+		url:'/update',
+		method : 'POST',
+		data: {
+			id: id,
+			value: JSON.stringify(modifier)
+		}
+	}).done(function(e){
+		console.log(e);
+	});
+});
 afficher();
+
+
+
 
 $('#text').keyup(function(){
 
--- server.js
+++ server.js
@@ -9,60 +9,64 @@
 	var tableau = [];
 
     // Champs à respecter(validator)
-	function toAlpha(string){
-		return string.replace('-','').split(" ").join('');
-	}
+    function toAlpha(string){
+    	return string.replace('-','').split(" ").join('');
+    }
 
     
-	function validate(coordinate){
-		var errors = {};
+    function validate(coordinate){
+    	var errors = {};
 
-		if(!validator.isAlpha(toAlpha(coordinate.titre))){
-			errors.titre = "Pas de caractéres spéciaux !";
-		}
-		if(!validator.isAlpha(toAlpha(coordinate.text))){
-			errors.text = "Pas de caractéres spéciaux !";
-		}
+    	if(!validator.isAlpha(toAlpha(coordinate.titre))){
+    		errors.titre = "Pas de caractéres spéciaux !";
+    	}
+    	if(!validator.isAlpha(toAlpha(coordinate.text))){
+    		errors.text = "Pas de caractéres spéciaux !";
+    	}
 
-		if(validator.isEmpty(coordinate.titre)){
-			errors.titre = "Champs obligatoires!";
-		}
-
-
-		if(validator.isEmpty(coordinate.text)){
-			errors.text = "Champs obligatoires!";
-		}
+    	if(validator.isEmpty(coordinate.titre)){
+    		errors.titre = "Champs obligatoires!";
+    	}
 
 
-		return errors;
+    	if(validator.isEmpty(coordinate.text)){
+    		errors.text = "Champs obligatoires!";
+    	}
 
-	}
 
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(bodyParser.json());
+    	return errors;
 
-	app.use(express.static('./'));
-	app.set('views', './views');
-	app.set('view engine', 'jade');
+    }
 
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    app.use(express.static('./'));
+    app.set('views', './views');
+    app.set('view engine', 'jade');
+
+    // app.get('/ ', function(req, res){
+    // 	res.render('connexion');
+    // });
+ 
 
     //Route page admin 
-	app.get('/', function (req, res) {
-		res.render('admin', {errors : {}});
-	});
+    app.get('/', function (req, res) {
+    	res.render('admin', {errors : {}});
+    });
 
 
     //Route post pour afficher les erreurs et les pages + nodefs
-	app.post('/article', function ( req, res){
+    app.post('/article', function ( req, res){
 
-		nodefs.readFile('stockage.json', 'utf8', function(err, data){
-			if(err) {
-				throw err;
+    	nodefs.readFile('stockage.json', 'utf8', function(err, data){
+    		if(err) {
+    			throw err;
 
-			}
-			tableau = JSON.parse(data);
-			console.log(content);
-		});
+    		}
+    		tableau = JSON.parse(data);
+    		console.log(content);
+    	});
 		//console.log(req.body);
 		var content = req.body;
 		content.id = uuidV1();
@@ -83,6 +87,9 @@
 			res.render('admin', {
 				titre: req.body.titre,
 				text: req.body.text,
+				id : req.body.id,
+				input : req.body.inputModif,
+				textarea : req.body.textareaModif,
 				errors: validate(req.body)
 
 			});
@@ -92,25 +99,64 @@
     
 
     //Afficher titre dans page admin 
-	app.get('/afficher', function (req, res){
-    res.send(JSON.stringify(tableau));
- 	});
+    app.get('/afficher', function (req, res){
+    	res.send(JSON.stringify(tableau));
+    });
     
     //Supprimer un article
- 	app.get('/delete', function(req, res){
 
-	var id = (req.query._id); 
+    app.post('/delete', function(req, res) {
+    	var id = (req.body.id);
 
-	for (var i = 0; i < tableau.length; i++){
-		if (tableau[i]._id == id) {
-			tableau.splice(i,1);
-			continue;
-		}
-	}
- 	});
+    	for (var i = 0; i < tableau.length; i++) {
+    		if(tableau[i].id == id){
+    			tableau.splice(i,1);
+    			continue;
+    		}
+    	}
+    	nodefs.writeFileSync('stockage.json', JSON.stringify(tableau), 'UTF-8');
+    	res.send('stockage.json supprimé...');
+    });
+
+    app.post('/update', function(req, res){
+
+    	var modifier = (req.body.id); 
+
+    	for (var i = 0; i < tableau.length; i++){
+    		if (tableau[i].id == modifier) {
+    			tableau[i] = JSON.parse(req.body.value);
+    			continue;
+    		}
+    	}
+    	nodefs.writeFileSync('stockage.json', JSON.stringify(tableau), 'UTF-8');
+    	res.send('stockage.json modifié...');
+    });
+
+    app.post("/Login", function (req, res) {
+    	var Obj = { "Username": req.body.username, "Password": req.body.password, "error": null };
+    	if (!req.body.Username && !req.body.password) {
+    		Obj.error = "User name is required";
+    		res.redirect("admin", Obj);
+    	} else if (req.body.username == req.session.username) {
+    // User has not changed Username, accept it as-is
+    res.redirect("/");
+} else if (!req.body.username.match(/^[a-zA-Z0-9\-_]{3,}$/)) {
+	Obj.error = "User name must have at least 3 alphanumeric characters";
+	res.render("admin", Obj);
+} else {
+    // Validate if Username is free
+    if (UsernameIsAlreadyUsed) {
+    	Obj.error = "User name is already used by someone else";
+    	res.render("index", Obj);
+    } else {
+    	req.session.username = req.body.username;
+    	res.render("admin");
+    }
+}
+});
 
 
 
-	app.listen(3000, function() {
-		console.log('App listening on port 3000!');
-	});
+    app.listen(3000, function() {
+    	console.log('App listening on port 3000!');
+    });
